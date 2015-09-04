4a1a19c97ca7ef7f8416b04dcc850198f1ef9a44
Tyson Bailey <onaclov2000+github@gmail.com>
2015-09-03 19:39:08 -0600

    Update SVMClassifier.py

--- SimpleCV/MachineLearning/SVMClassifier.py
+++ SimpleCV/MachineLearning/SVMClassifier.py
@@ -141,69 +141,6 @@ class SVMClassifier(Classifier):
         self.mFeatureExtractors = extractors
         return None
 
-    def _trainPath(self,path,className,subset,disp,verbose):
-        count = 0
-        files = []
-        for ext in IMAGE_FORMATS:
-            files.extend(glob.glob( os.path.join(path, ext)))
-        if(subset > 0):
-            nfiles = min(subset,len(files))
-        else:
-            nfiles = len(files)
-        badFeat = False
-        for i in range(nfiles):
-            infile = files[i]
-            if verbose:
-                print "Opening file: " + infile
-            img = Image(infile)
-            featureVector = []
-            for extractor in self.mFeatureExtractors:
-                feats = extractor.extract(img)
-
-                if( feats is not None ):
-                    featureVector.extend(feats)
-                else:
-                    badFeat = True
-
-            if(badFeat):
-                badFeat = False
-                continue
-            featureVector.extend([className])
-            self.mDataSetRaw.append(featureVector)
-            text = 'Training: ' + className
-            self._WriteText(disp,img,text,Color.WHITE)
-            count = count + 1
-            del img
-        return count
-
-    def _trainImageSet(self,imageset,className,subset,disp,verbose):
-        count = 0
-        badFeat = False
-        if (subset>0):
-            imageset = imageset[0:subset]   
-        for img in imageset:
-            if verbose:
-                print "Opening file: " + img.filename
-            featureVector = []
-            for extractor in self.mFeatureExtractors:
-                feats = extractor.extract(img)
-                if( feats is not None ):
-                    featureVector.extend(feats)
-                else:
-                    badFeat = True
-                    
-            if(badFeat):
-                badFeat = False
-                continue
-            
-            featureVector.extend([className])
-            self.mDataSetRaw.append(featureVector)
-            text = 'Training: ' + className
-            self._WriteText(disp,img,text,Color.WHITE)
-            count = count + 1
-            del img
-        return count
-
     def train(self,images,classNames,disp=None,subset=-1,savedata=None,verbose=True):
         """
         Train the classifier.
