e2650aa86e43f12f638e359f7a77326754c1c46c
Tyson Bailey <onaclov2000+github@gmail.com>
2015-09-03 19:56:41 -0600

    Update KNNClassifier.py

--- SimpleCV/MachineLearning/KNNClassifier.py
+++ SimpleCV/MachineLearning/KNNClassifier.py
@@ -75,15 +75,6 @@ class KNNClassifier(Classifier):
         Note that training and testing will need to be redone.
         """
         self.mDistType = self.mDistDict[dist]
-        
-    def __setstate__(self, mydict):
-        self.__dict__ = mydict
-        colNames = []
-        for extractor in self.mFeatureExtractors:
-            colNames.extend(extractor.getFieldNames())
-        self.mOrangeDomain = orange.Domain(map(orange.FloatVariable,colNames),orange.EnumVariable("type",values=self.mClassNames))
-        self.mDataSetOrange = orange.ExampleTable(self.mOrangeDomain,self.mDataSetRaw)
-
 
     def classify(self, image):
         """
@@ -112,69 +103,6 @@ class KNNClassifier(Classifier):
         """
         self.mFeatureExtractors = extractors
         return None
-
-    def _trainPath(self,path,className,subset,disp,verbose):
-        count = 0
-        files = []
-        for ext in IMAGE_FORMATS:
-            files.extend(glob.glob( os.path.join(path, ext)))
-        if(subset > 0):
-            nfiles = min(subset,len(files))
-        else:
-            nfiles = len(files)
-        badFeat = False
-        for i in range(nfiles):
-            infile = files[i]
-            if verbose:
-                print "Opening file: " + infile
-            img = Image(infile)
-            featureVector = []
-            for extractor in self.mFeatureExtractors:
-                feats = extractor.extract(img)
-                if( feats is not None ):
-                    featureVector.extend(feats)
-                else:
-                    badFeat = True
-
-            if(badFeat):
-                badFeat = False
-                continue
-
-            featureVector.extend([className])
-            self.mDataSetRaw.append(featureVector)
-            text = 'Training: ' + className
-            self._WriteText(disp,img,text,Color.WHITE)
-            count = count + 1
-            del img
-        return count
-
-    def _trainImageSet(self,imageset,className,subset,disp,verbose):
-        count = 0
-        badFeat = False
-        if (subset>0):
-            imageset = imageset[0:subset]   
-        for img in imageset:
-            if verbose:
-                print "Opening file: " + img.filename
-            featureVector = []
-            for extractor in self.mFeatureExtractors:
-                feats = extractor.extract(img)
-                if( feats is not None ):
-                    featureVector.extend(feats)
-                else:
-                    badFeat = True
-                    
-            if(badFeat):
-                badFeat = False
-                continue
-            
-            featureVector.extend([className])
-            self.mDataSetRaw.append(featureVector)
-            text = 'Training: ' + className
-            self._WriteText(disp,img,text,Color.WHITE)
-            count = count + 1
-            del img
-        return count
 
     def train(self,images,classNames,disp=None,subset=-1,savedata=None,verbose=True):
         """
@@ -255,9 +183,6 @@ class KNNClassifier(Classifier):
                     print ("%s" + ("\t%i" * len(classes))) % ((className, ) + tuple(classConfusions))
         return [good, bad, confusion]
 
-
-
-
     def test(self,images,classNames,disp=None,subset=-1,savedata=None,verbose=True):
         """
         Test the classifier.
@@ -320,95 +245,3 @@ class KNNClassifier(Classifier):
                 print ("%s" + ("\t%i" * len(classes))) % ((className, ) + tuple(classConfusions))
 
         return [good, bad, confusion]
-
-    def _testPath(self,path,className,dataset,subset,disp,verbose):
-        count = 0
-        correct = 0
-        badFeat = False
-        files = []
-        for ext in IMAGE_FORMATS:
-            files.extend(glob.glob( os.path.join(path, ext)))
-        if(subset > 0):
-            nfiles = min(subset,len(files))
-        else:
-            nfiles = len(files)
-        for i in range(nfiles):
-            infile = files[i]
-            if verbose:
-                print "Opening file: " + infile
-            img = Image(infile)
-            featureVector = []
-            for extractor in self.mFeatureExtractors:
-                feats = extractor.extract(img)
-                if( feats is not None ):
-                    featureVector.extend(feats)
-                else:
-                    badFeat = True
-            if( badFeat ):
-                del img
-                badFeat = False
-                continue
-            featureVector.extend([className])
-            dataset.append(featureVector)
-            test = orange.ExampleTable(self.mOrangeDomain,[featureVector])
-            c = self.mClassifier(test[0])
-            testClass = test[0].getclass()
-            if(testClass==c):
-                text =  "Classified as " + str(c)
-                self._WriteText(disp,img,text, Color.GREEN)
-                correct = correct + 1
-            else:
-                text =  "Mislassified as " + str(c)
-                self._WriteText(disp,img,text, Color.RED)
-            count = count + 1
-            del img
-
-        return([dataset,count,correct])
-
-    def _testImageSet(self,imageset,className,dataset,subset,disp,verbose):
-        count = 0
-        correct = 0
-        badFeat = False
-        if(subset > 0):
-            imageset = imageset[0:subset]
-        for img in imageset:
-            if verbose:
-                print "Opening file: " + img.filename
-            featureVector = []
-            for extractor in self.mFeatureExtractors:
-                feats = extractor.extract(img)
-                if( feats is not None ):
-                    featureVector.extend(feats)
-                else:
-                    badFeat = True
-            if( badFeat ):
-                del img
-                badFeat = False
-                continue 
-            featureVector.extend([className])
-            dataset.append(featureVector)
-            test = orange.ExampleTable(self.mOrangeDomain,[featureVector])
-            c = self.mClassifier(test[0])
-            testClass = test[0].getclass()
-            if(testClass==c):
-                text =  "Classified as " + str(c)
-                self._WriteText(disp,img,text, Color.GREEN)
-                correct = correct + 1
-            else:   
-                text =  "Mislassified as " + str(c)
-                self._WriteText(disp,img,text, Color.RED)
-            count = count + 1
-            del img
-            
-        return([dataset,count,correct])
-
-    def _WriteText(self, disp, img, txt,color):
-        if(disp is not None):
-            txt = ' ' + txt + ' '
-            img = img.adaptiveScale(disp.resolution)
-            layer = DrawingLayer((img.width,img.height))
-            layer.setFontSize(60)
-            layer.ezViewText(txt,(20,20),fgcolor=color)
-            img.addDrawingLayer(layer)
-            img.applyLayers()
-            img.save(disp)
