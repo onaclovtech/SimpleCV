44e9a94d455a37439cf89c451b67f66c859c712b
Tyson Bailey <onaclov2000+github@gmail.com>
2015-09-13 15:37:25 -0600

    Update NeuralNetworkClassifier.py

--- SimpleCV/MachineLearning/NeuralNetworkClassifier.py
+++ SimpleCV/MachineLearning/NeuralNetworkClassifier.py
@@ -231,7 +231,8 @@ class NeuralNetworkClassifier:
         if(savedata is not None):
             orange.saveTabDelimited (savedata, self.mDataSetOrange)
 
-        self.mClassifier = orange.neuralNetworkLearner(self.mDataSetOrange)
+        self.mClassifier = Orange.classification.neural"[]
+        \.NeuralNetworkLearner(self.mDataSetOrange)
         correct = 0
         incorrect = 0
         for i in range(count):
